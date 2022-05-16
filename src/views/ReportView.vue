3b28755ee09e19b0b9ca34917f1416bc8f5d7f1c
matiassemelman <matias.semelman@protonmail.com>
2022-05-16 16:12:05 -0300

    Feature: Add data iterator header

--- src/views/ReportView.vue
+++ src/views/ReportView.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+   <v-container fluid>
+  
+    <v-data-iterator
+      :items="desserts"
+      item-key="name"
+      :items-per-page="4"
+      hide-default-footer
+    >
+    <template v-slot:header>
+        <v-toolbar
+          class="mb-2"
+          
+          flat
+        >
+          <v-toolbar-title class="primary--text headline mr-8">My Report</v-toolbar-title>
+          
+          <v-icon small>content_copy</v-icon>
+          <v-icon small>share</v-icon>
+          <v-spacer></v-spacer>
+        <h1>Hello</h1>
+        </v-toolbar>
+      </template>
+     
+    </v-data-iterator>
+  </v-container>
 </template>
